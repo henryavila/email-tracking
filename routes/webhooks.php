f1ecc40f08678fe2a968fe31d0eb20dbe561cd14
Henry Ávila <henry.avila@crcmg.org.br>
2024-08-13 13:52:40 -0300

    Fix middleware call

--- routes/webhooks.php
+++ routes/webhooks.php
@@ -3,9 +3,10 @@
 declare(strict_types=1);
 
 use HenryAvila\EmailTracking\Controllers\MailgunWebhookController;
+use HenryAvila\EmailTracking\Middleware\Webhooks\MailgunWebhookMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware([MailgunWebhookController::class])->prefix('webhooks')->group(function () {
+Route::middleware([MailgunWebhookMiddleware::class])->prefix('webhooks')->group(function () {
     Route::post('mailgun', MailgunWebhookController::class)
         ->name('email-tracking.webhooks.mailgun');
 });
